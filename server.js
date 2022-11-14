d6360e37cd1367b104accd2f4cb7d74f753e028c
Hp <Hp@MacBook-Pro-2.local>
2022-11-14 11:35:01 +1100

    thtird

--- server.js
+++ server.js
@@ -5,29 +5,12 @@ const PORT = 8000;
 import morgan from "morgan";
 //middleware
 app.use(morgan("dev"));
+app.use(express.json());
 
-// api endpoints
 
-// workflow : CRUD
-// C(create) => receive new task and store in the database
-app.post("/api/v1/task", (req, res) => {
-  res.json({ message: "todo Post method" });
-});
-
-// R(Read) => read data from data base and return to the client
-app.get("/api/v1/task", (req, res) => {
-  res.json({ message: "todo get method" });
-});
-
-// U(Update) => update some information of existing data int he database and respond client accordingly
-app.put("/api/v1/task", (req, res) => {
-  res.json({ message: "todo put method" });
-});
-
-//D(Delete) => Delete data(s) from database and response client accordingly
-app.delete("/api/v1/task", (req, res) => {
-  res.json({ message: "todo delete method" });
-});
+// api endpoint
+import taskRouter from './src/taskRouter.js'
+app.use('/api/v1/task', taskRouter);
 
 app.listen(PORT, (error) => {
   error
